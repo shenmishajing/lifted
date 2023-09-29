c2c5c4adba100d4fda7495be8174aae1f79ac0ad
shenmishajing <shenmishajing@gmail.com>
2023-09-29 09:15:07 +0800

    add distribute feature to benchmark

--- tools/models/benchmark.py
+++ tools/models/benchmark.py
@@ -1,8 +1,7 @@
 # load demo data
 import os
+import pickle
 import random
-import shutil
-from collections import defaultdict
 
 import numpy as np
 import torch
@@ -23,107 +22,92 @@ def get_random_seed():
     return random.randint(np.iinfo(np.uint32).min, np.iinfo(np.uint32).max)
 
 
-def parse_results(results):
-    for split in results:
-        result = {}
+def parse_results(data):
+    result = {}
+    result["seed"] = [d["seed"] for d in data]
+    for split in ["valid", "test"]:
+        result[split] = {}
         for metric in ["PR-AUC", "F1", "ROC-AUC"]:
-            result[metric] = torch.stack([k[metric] for k in results[split]])
+            result[split][metric] = torch.stack([d[split][metric] for d in data])
+            result[split][metric] = [
+                result[split][metric].mean().item(),
+                result[split][metric].std().item(),
+            ]
 
-        for metric in result:
-            result[metric] = [result[metric].mean().item(), result[metric].std().item()]
-
-        results[split] = result
-    return results
-
-
-def hint(datas, metrics, phase, n=30, output_path="results/details"):
-    results = defaultdict(list)
-    datasets = {k: TrialOutcomeDatasetBase(v) for k, v in datas.items()}
-
-    os.makedirs(output_path, exist_ok=True)
-    file = open(
-        f"results/details/{phase}_hint.txt",
-        "w",
-    )
-
-    for _ in trange(n):
-        seed = get_random_seed()
-        file.write(f"seed: {seed}\n")
-        seed_everything(seed)
-
-        model = HINT(highway_num_layer=2, epoch=1, lr=1e-3)
-        model.fit(datasets["train"], datasets["valid"])
-
-        for split in ["valid", "test"]:
-            preds = model.predict(datasets[split])
-            target = (
-                datas[split][["nctid", "label"]].set_index("nctid").to_dict("split")
-            )
-            target = {k: v[0] for k, v in zip(target["index"], target["data"])}
-            target = torch.tensor([target[k[0]] for k in preds])
-            preds = torch.tensor([k[1] for k in preds])
-            result = metrics(preds, target)
-            results[split].append(result)
-            metrics.reset()
-
-            file.write(f"{split}\t{result}\n")
-
-    file.close()
-
-    return results
+    return result
 
 
-def spot(
-    datas,
-    metrics,
+def fit_modal(
+    model_name,
     phase,
     n=30,
-    use_hint_hyperparameters=True,
+    world_size=1,
+    rank=0,
     output_path="results/details",
+    *args,
+    **kwargs,
 ):
-    results = defaultdict(list)
-    datasets = {k: TrialOutcomeDataset(v) for k, v in datas.items()}
+    output_path = os.path.join(output_path, phase, model_name)
 
-    os.makedirs(output_path, exist_ok=True)
-
-    for _ in trange(n):
-        file = open(
-            f"results/details/{phase}_spot{'_hint' if use_hint_hyperparameters else ''}.txt",
-            "a",
-        )
+    for i in trange(rank, n, world_size):
         seed = get_random_seed()
-        file.write(f"seed: {seed}\n")
         seed_everything(seed)
 
-        if use_hint_hyperparameters:
-            model = SPOT(
-                epochs=5,
-                learning_rate=1e-3,
-                weight_decay=0,
-                seed=seed,
-                output_dir=f"./checkpoints/spot/phase_{phase}",
-            )
-        else:
-            model = SPOT(seed=seed, output_dir=f"./checkpoints/spot/phase_{phase}")
+        result = model_map[model_name](
+            *args,
+            **kwargs,
+            seed=seed,
+            model_name=model_name,
+            output_path=os.path.join(output_path, str(i)),
+        )
+        result["seed"] = seed
+        pickle.dump(result, open(os.path.join(output_path, f"{i}.pkl"), "wb"))
 
-        model.fit(datasets["train"], datasets["valid"])
 
-        for split in ["valid", "test"]:
-            print(split)
-            preds = model.predict(datasets[split])
-            target = torch.tensor(preds["label"])
-            preds = torch.tensor(preds["pred"][:, 0])
-            result = metrics(preds, target)
-            results[split].append(result)
-            metrics.reset()
+def hint(datasets, metrics, datas, *args, **kwargs):
+    model = HINT(highway_num_layer=2, epoch=1, lr=1e-3)
+    model.fit(datasets["train"], datasets["valid"])
 
-            file.write(f"{split}\t{result}\n")
+    result = {}
+    for split in ["valid", "test"]:
+        preds = model.predict(datasets[split])
+        target = datas[split][["nctid", "label"]].set_index("nctid").to_dict("split")
+        target = {k: v[0] for k, v in zip(target["index"], target["data"])}
+        target = torch.tensor([target[k[0]] for k in preds])
+        preds = torch.tensor([k[1] for k in preds])
+        result[split] = metrics(preds, target)
+        metrics.reset()
 
-        shutil.rmtree(f"./checkpoints/spot/phase_{phase}")
+    return result
 
-        file.close()
 
-    return results
+def spot(datasets, metrics, model_name, seed, output_path, *args, **kwargs):
+    if model_name == "spot_hint":
+        model = SPOT(
+            epochs=5,
+            learning_rate=1e-3,
+            weight_decay=0,
+            seed=seed,
+            output_dir=os.path.join(output_path, "checkpoints"),
+        )
+    else:
+        model = SPOT(seed=seed, output_dir=os.path.join(output_path, "checkpoints"))
+
+    model.fit(datasets["train"], datasets["valid"])
+
+    result = {}
+    for split in ["valid", "test"]:
+        print(split)
+        preds = model.predict(datasets[split])
+        target = torch.tensor(preds["label"])
+        preds = torch.tensor(preds["pred"][:, 0])
+        result[split] = metrics(preds, target)
+        metrics.reset()
+
+    return result
+
+
+model_map = {"hint": hint, "spot": spot, "spot_hint": spot}
 
 
 def argparse():
@@ -132,7 +116,10 @@ def argparse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="spot_hint,spot,hint")
     parser.add_argument("--phase", type=str, default="I,II,III")
+    parser.add_argument("--output-path", type=str, default="results")
     parser.add_argument("--n", type=int, default=30)
+    parser.add_augment("--world-size", type=int, default=1)
+    parser.add_augment("--rank", type=int, default=0)
     return parser.parse_args()
 
 
@@ -148,12 +135,7 @@ def main():
 
     args = argparse()
 
-    models = args.model.split(",")
-    phases = args.phase.split(",")
-    save_path = "results/summarization"
-    os.makedirs(save_path, exist_ok=True)
-
-    for phase in phases:
+    for phase in args.phase.split(","):
         datas = {
             s: load_trial_outcome_data(phase=phase, split=s)["data"]
             for s in ["train", "valid", "test"]
@@ -162,21 +144,54 @@ def main():
         for split in datas:
             datas[split] = datas[split].dropna(axis=0, subset=["criteria"])
 
-        for model in models:
-            if model == "hint":
-                results = hint(datas, metrics, phase, args.n)
-            elif model == "spot":
-                results = spot(datas, metrics, phase, args.n, False)
-            elif model == "spot_hint":
-                results = spot(datas, metrics, phase, n=args.n)
+        for model in args.model.split(","):
+            if model == "summary":
+                data_path = os.path.join(args.output_path, "details", phase)
+                output_path = os.path.join(args.output_path, "summary")
 
-            results = parse_results(results)
+                res_string = ""
+                for model_name in os.listdir(data_path):
+                    results = set(
+                        [
+                            int(res.removesuffix(".pkl"))
+                            for res in os.listdir(os.path.join(data_path, model_name))
+                        ]
+                    )
+                    results = set(range(args.n)) - results
+                    if len(results) > 0:
+                        print(f"{model_name} does not complete: {results}")
+                        continue
 
-            res_string = ""
-            for metric in ["PR-AUC", "F1", "ROC-AUC"]:
-                res_string += f'& $ {results["test"][metric][0]:.3g} \pm {results["test"][metric][1]:.3g} $'
-            res_string += "\\\\\n"
-            open(os.path.join(save_path, f"{phase}_{model}.txt"), "w").write(res_string)
+                    results = [
+                        pickle.load(
+                            open(os.path.join(data_path, model_name, f"{i}.pkl"), "rb")
+                        )
+                        for i in range(args.n)
+                    ]
+                    results = parse_results(results)
+
+                    res_string += f"{model_name} "
+                    for metric in ["PR-AUC", "F1", "ROC-AUC"]:
+                        res_string += f'& $ {results["test"][metric][0]:.3g} \pm {results["test"][metric][1]:.3g} $'
+                    res_string += "\\\\\n"
+                open(os.path.join(output_path, f"{phase}.txt"), "w").write(res_string)
+            else:
+                if model == "hint":
+                    datasets = {k: TrialOutcomeDatasetBase(v) for k, v in datas.items()}
+                else:
+                    datasets = {k: TrialOutcomeDataset(v) for k, v in datas.items()}
+
+                fit_modal(
+                    model,
+                    phase,
+                    args.n,
+                    args.world_size,
+                    args.rank,
+                    output_path=os.path.join(args.output_path, "details"),
+                    datasets=datasets,
+                    metrics=metrics,
+                    datas=datas,
+                )
 
 
 if __name__ == "__main__":
