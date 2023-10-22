02088a2839b6bcef1be7a46f22732e8bc2cbfe7a
shenmishajing <shenmishajing@gmail.com>
2023-10-22 22:37:07 +0800

    add predict hidden state feature

--- project/models/base.py
+++ project/models/base.py
@@ -1,10 +1,17 @@
+import os
+import pickle
+from collections import defaultdict
+
+import pandas as pd
 import torch
 from lightning_template import LightningModule as _LightningModule
 
 
 class LightningModule(_LightningModule):
-    def __init__(self, bootstrap_num=20, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, bootstrap_num=20, predict_tasks=None, *args, **kwargs) -> None:
+        if predict_tasks is None:
+            predict_tasks = ["hidden_state"]
+        super().__init__(predict_tasks=predict_tasks, *args, **kwargs)
         self.bootstrap_num = bootstrap_num
 
     def update_evaluator(self, evaluator, *args, **kwargs):
@@ -54,3 +61,86 @@ class LightningModule(_LightningModule):
                 result[f"{key}_std"] = data.std()
 
         return result
+
+    def on_predict_epoch_start(self) -> None:
+        if self.trainer.ckpt_path:
+            output_path = os.path.join(
+                "results",
+                "visualization",
+                os.path.basename(
+                    os.path.dirname(
+                        os.path.dirname(os.path.dirname(self.trainer.ckpt_path))
+                    )
+                ),
+            )
+        else:
+            output_path = None
+
+        for name in self.predict_tasks:
+            if self.predict_tasks[name] is None:
+                if output_path is None:
+                    raise ValueError(
+                        "predict_path is None, please set predict_path or pass ckpt_path"
+                    )
+
+                self.predict_tasks[name] = output_path
+
+            self.predict_tasks[name] = os.path.join(self.predict_tasks[name], name)
+            self.rm_and_create(self.predict_tasks[name])
+
+        self.hidden_states = defaultdict(list)
+
+    def predict_forward(self, *args, **kwargs):
+        return self(*args, **kwargs)
+
+    def predict_hidden_state(
+        self, batch, *args, hidden_state_dict, metric_dict, **kwargs
+    ):
+        self.hidden_states["moe_weights"].append(hidden_state_dict["moe_weights"].cpu())
+        self.hidden_states["preds"].append(metric_dict["preds"].cpu())
+        self.hidden_states["target"].append(metric_dict["target"].cpu())
+        self.hidden_states["error"].append(
+            (metric_dict["preds"] - metric_dict["target"]).abs().cpu()
+        )
+        self.hidden_states["idx"].extend(batch["idx"])
+
+    def on_predict_epoch_end(self) -> None:
+        for key in self.hidden_states:
+            if key == "idx":
+                continue
+            self.hidden_states[key] = torch.cat(self.hidden_states[key], dim=0)
+
+        pickle.dump(
+            self.hidden_states,
+            open(
+                os.path.join(self.predict_tasks["hidden_state"], "all.pkl"),
+                "wb",
+            ),
+        )
+
+        idx = self.hidden_states["error"].topk(10, dim=0).indices
+        for key in self.hidden_states:
+            if key == "idx":
+                self.hidden_states[key] = [self.hidden_states[key][i] for i in idx]
+            else:
+                self.hidden_states[key] = self.hidden_states[key][idx]
+
+        pickle.dump(
+            self.hidden_states,
+            open(
+                os.path.join(self.predict_tasks["hidden_state"], "top10.pkl"),
+                "wb",
+            ),
+        )
+
+        data = pd.read_csv(
+            os.path.join(
+                self.trainer.datamodule.dataset.data_prefix["data_path"],
+                f"{self.trainer.datamodule.dataset.ann_file_name}.csv",
+            )
+        ).iloc[self.hidden_states["idx"]]
+
+        data.to_csv(
+            os.path.join(self.predict_tasks["hidden_state"], "top10.csv"),
+            index=False,
+        )
